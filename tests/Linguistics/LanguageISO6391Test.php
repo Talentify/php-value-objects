30bf73517f598ab8ade799696f91ae36d43bbf43
Joilson Rezende <joilson560@gmail.com>
2019-07-29 16:57:57 -0300

    Add LanguageISO6391

---
+++ tests/Linguistics/LanguageISO6391Test.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talentify\ValueObject\Linguistics;
+
+use InvalidArgumentException;
+use Talentify\ValueObject\ValueObjectTestCase;
+
+class LanguageISO6391Test extends ValueObjectTestCase
+{
+    public static function getClassName() : string
+    {
+        return LanguageISO6391::class;
+    }
+
+    public function sameValueDataProvider() : array
+    {
+        return [
+            [new LanguageISO6391('en'), new LanguageISO6391('en')],
+            [new LanguageISO6391('English', 'en'), new LanguageISO6391('English', 'en')],
+        ];
+    }
+
+    public function differentValueDataProvider() : array
+    {
+        return [
+            [new LanguageISO6391('Portuguese'), new LanguageISO6391('English')],
+            [new LanguageISO6391('pt'), new LanguageISO6391('Portuguese', 'pt')],
+        ];
+    }
+
+    public function testSetIsoCodeWhenNameIsIsoCode() : void
+    {
+        $isoLanguage = new LanguageISO6391('en');
+
+        $this->assertEquals('en', $isoLanguage->getName());
+        $this->assertEquals('en', $isoLanguage->getIsoCode());
+    }
+
+    public function testNameValidation() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new LanguageISO6391('');
+    }
+
+    public function testIsoCodeValidation() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new LanguageISO6391('Portuguese', 'Portuguese');
+    }
+}
